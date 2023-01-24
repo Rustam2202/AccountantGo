fc116915911e2cd15ec64a3f6532d576e91a3fae
Rustam2202 <91486552+Rustam2202@users.noreply.github.com>
2023-01-24 14:20:51 +0300

    calendar func

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	acc "accounter/app"
+	"time"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	xwidget "fyne.io/x/fyne/widget"
 )
 
 func main() {
@@ -27,11 +30,14 @@ func main() {
 	addBtn := widget.NewButton("Add", func() {
 		// income := incomeEntry.Text
 	})
-	subBtn:=widget.NewButton("Sub",func() {
+	subBtn := widget.NewButton("Sub", func() {
 	})
+
+	calendar:=calendar()
 
 	w.SetContent(
 		container.NewVBox(
+			&calendar,
 			enterOperText,
 			container.NewGridWithColumns(4,
 				emptyLabel, sumLabel, dateLabel, emptyLabel,
@@ -42,4 +48,26 @@ func main() {
 	)
 	w.ShowAndRun()
 	acc.Calc()
+}
+
+type date struct {
+	instruction *widget.Label
+	dateChosen  *widget.Label
+}
+
+func(d*date)onSelected(t time.Time){
+d.instruction.SetText("Date Selected:")
+d.dateChosen.SetText(t.Format("Mon 2 Jan 2006"))
+}
+
+func calendar() fyne.Container{
+	i := widget.NewLabel("Choose a date")
+	i.Alignment = fyne.TextAlignCenter
+	l := widget.NewLabel("")
+	l.Alignment = fyne.TextAlignCenter
+	d := &date{instruction: i, dateChosen: l}
+	startingDate := time.Now()
+	calendar := xwidget.NewCalendar(startingDate, d.onSelected)
+	c:=container.NewVBox(i,l,calendar)
+	return *c
 }
