d8da294e10087854d5615657abace6947192b2c3
Rustam2202 <rustam2202@yandex.ru>
2023-01-29 10:08:29 +0300

    table shows in main window

--- gui/period.go
+++ gui/period.go
@@ -6,11 +6,11 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
-	
+
 	"fyne.io/fyne/v2/widget"
 )
 
-func PeriodDates(dataBase *db.Database, win fyne.Window) *fyne.Container {
+func PeriodDates(cont *fyne.Container, dataBase *db.Database, win fyne.Window) *fyne.Container {
 
 	empty := widget.NewLabel("")
 	labelPeriod := widget.NewLabel("Enter period:")
@@ -37,29 +37,25 @@ func PeriodDates(dataBase *db.Database, win fyne.Window) *fyne.Container {
 	toBtn := CalendarBtn(dateToBind, win)
 
 	confirmBtn := widget.NewButton("Show", func() {
-		w := fyne.CurrentApp().NewWindow("Hello")
-		w.SetContent(MakeTable(dataBase))
-		w.Resize(fyne.NewSize(400,400))
-		w.Show()
-
+		/*
+			w := fyne.CurrentApp().NewWindow("Hello")
+			w.SetContent(MakeTable(dataBase))
+			w.Resize(fyne.NewSize(400,400))
+			w.Show()
+		*/
+		table:=MakeTable(dataBase)
+	//	table.Move(fyne.NewPos(0,100))
+		cont.AddObject(table)
+	//	cont.Add(table)
+		cont.Show()
 		//t := MakeTable(dataBase)
 		//t.Resize(fyne.NewSize(100, 100))
 		//dialog.ShowCustom("title", "dissmis", MakeTable(dataBase), win)
 	})
+	// confirmBtn.Resize(fyne.NewSize(100,100))
 
-	table := widget.NewTable(
-		func() (int, int) {
-			return 5, 5
-		},
-		func() fyne.CanvasObject {
-			return widget.NewLabel("")
-		},
-		func(tci widget.TableCellID, co fyne.CanvasObject) {
-			co.(*widget.Label).SetText("text")
-		},
-	)
-	table.MinSize()
 	c := container.NewVBox(
+		// 3,
 		container.NewGridWithColumns(6,
 			empty, labelPeriod, empty, empty, labelMonth, empty,
 			fromLabel, dateFromEntry, fromBtn, empty, monthEntry, monthLabel,
@@ -67,5 +63,6 @@ func PeriodDates(dataBase *db.Database, win fyne.Window) *fyne.Container {
 		),
 		confirmBtn,
 	)
+	// c.Add(container.NewMax(MakeTable(dataBase)))
 	return c
 }
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	db "accounter/db"
 	gui "accounter/gui"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 var DataBase db.Database
@@ -16,12 +18,63 @@ func main() {
 
 	a := app.New()
 	w := a.NewWindow("Accounter Manager")
-	w.Resize(fyne.NewSize(800, 600))
+	w.Resize(fyne.NewSize(400, 600))
+
+	tableHeader := widget.NewTable(
+		func() (int, int) { return 1, 5 },
+		func() fyne.CanvasObject { return widget.NewLabel("") },
+		func(tci widget.TableCellID, co fyne.CanvasObject) {
+			label := co.(*widget.Label)
+			switch tci.Col {
+			case 0:
+				label.SetText("No")
+			case 1:
+				label.SetText("Date")
+			case 2:
+				label.SetText("Income")
+			case 3:
+				label.SetText("Spend")
+			case 4:
+				label.SetText("Comment")
+			default:
+			}
+		},
+	)
+	tableHeader.SetColumnWidth(0, 100)
+	tableHeader.SetColumnWidth(1, 100)
+	tableHeader.SetColumnWidth(2, 100)
+	tableHeader.SetColumnWidth(3, 100)
+	tableHeader.SetColumnWidth(4, 100)
+	tableHeader.SetRowHeight(0,100)
+
+	table := widget.NewTable(
+		func() (int, int) { return 0, 0 },
+		func() fyne.CanvasObject { return widget.NewLabel("Cell") },
+		func(tci widget.TableCellID, co fyne.CanvasObject) {},
+	)
+	table.SetColumnWidth(0, 100)
+	table.SetColumnWidth(1, 100)
+	table.SetColumnWidth(2, 100)
+	table.SetColumnWidth(3, 100)
+	table.SetColumnWidth(4, 100)
+
+	table.Resize(fyne.NewSize(100, 100))
+	//h:=fyne.Size{1000,1000}
+	//table=table.CreateRenderer().MinSize().Height
+	ContWithTable := container.NewMax(table)
+	ContWithTable.Hide()
+	//contWithTable.Show()
 
 	w.SetContent(
-		container.NewVBox(
+
+		container.NewAdaptiveGrid(
+			1,
 			gui.AddOperation(&DataBase, w),
-			gui.PeriodDates(&DataBase, w),
+			gui.PeriodDates(ContWithTable, &DataBase, w),
+		//	container.NewVBox(
+				tableHeader,
+				ContWithTable,
+		//	),
 
 		//	gui.MakeTable(&DataBase),
 		),
